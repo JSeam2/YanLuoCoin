6f6270477927ef622e4cb78fb2fde01ace2ee86e
JSeam2 <joshia_seam@mymail.sutd.edu.sg>
2018-08-23 02:19:26 +0800

    deployed token to rinkeby

--- truffle.js
+++ truffle.js
@@ -1,16 +1,3 @@
-/*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
- * function when declaring them. Failure to do so will cause commands to hang. ex:
- * ```
- * mainnet: {
- *     provider: function() { 
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
- *     },
- *     network_id: '1',
- *     gas: 4500000,
- *     gasPrice: 10000000000,
- *   },
- */
  var HDWalletProvider = require("truffle-hdwallet-provider");
  var secrets = require("./secrets");
  var rinkebyProvider = new HDWalletProvider(secrets.mnemonic, secrets.infuraURL);
